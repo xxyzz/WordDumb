7d4ec6458e97567f800034fa21707211dd6a0c99
xxyzz <gitpull@protonmail.com>
2021-02-22 13:05:18 +0800

    fix test

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-from pathlib import Path
 from filecmp import cmp
 
 from calibre.library import db
-from calibre.utils.config import config_dir
+from calibre_plugins.worddumb.database import get_ll_path
 from calibre_plugins.worddumb.metadata import check_metadata
 from calibre_plugins.worddumb.parse_job import do_job
-from calibre_plugins.worddumb.database import get_ll_path
 
 db = db('~/Calibre Library').new_api
 book_ids = db.all_book_ids()
-plugin_path = Path(config_dir).joinpath('plugins/WordDumb.zip')
-do_job(db, book_ids, plugin_path, None, None, None)
+do_job(db, book_ids, None, None, None)
 (_, asin, book_path, _) = check_metadata(db, next(iter(book_ids)), False)
 
 if asin != 'B003JTHWKU':

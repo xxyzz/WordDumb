b49340be18de5f58aa203776b810b2ecdd58e65a
xxyzz <gitpull@protonmail.com>
2021-02-22 12:57:11 +0800

    download numpy

--- unzip.py
+++ unzip.py
@@ -2,16 +2,18 @@
 
 import shutil
 import sys
-from pathlib import Path
 import zipfile
+from pathlib import Path
 
 from calibre.utils.config import config_dir
+from calibre_plugins.worddumb.config import prefs
 
-DB_VERSION = '1.1'
-WORDNET_VERSION = '3.0'
+DB_VERSION = '1.2'
+NUMPY_VERSION = '1.20.1'
+PLUGIN_PATH = Path(config_dir).joinpath('plugins/WordDumb.zip')
 
 
-def check_folder(plugin_path, folder_name, version, file_name, extract):
+def check_folder(folder_name, version, file_name, extract):
     extract_path = Path(config_dir).joinpath('plugins/'
                                              + folder_name + version)
     if not extract_path.is_dir():
@@ -20,36 +22,63 @@ def check_folder(plugin_path, folder_name, version, file_name, extract):
                 shutil.rmtree(f)  # delete old folder
 
         if extract:
-            unzip(plugin_path, file_name, extract_path)
+            unzip(file_name, extract_path, PLUGIN_PATH)
 
     return extract_path
 
 
-def unzip(plugin_path, file_name, extract_path):
-    with zipfile.ZipFile(plugin_path, 'r') as zf:
+def unzip(file_name, extract_path, zip_file_path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zf:
         for f in zf.namelist():
-            if file_name in f:
-                zf.extract(f, path=extract_path)
+            if not file_name or file_name in f:
+                zf.extract(f, extract_path)
 
 
-def install_libs(plugin_path):
-    for dir in zipfile.Path(plugin_path).joinpath('.venv/lib').iterdir():
-        sys.path.append(str(dir.joinpath('site-packages')))
+def install_libs():
+    for d in zipfile.Path(PLUGIN_PATH).joinpath('.venv/lib').iterdir():
+        sys.path.append(str(d.joinpath('site-packages')))
 
-    download_wordnet(plugin_path)
+    download_nltk_data()
+    if prefs['x-ray']:
+        download_numpy()
 
 
-def download_wordnet(plugin_path):
-    wordnet_path = check_folder(
-        plugin_path, 'worddumb-wordnet', WORDNET_VERSION, None, False)
+def download_nltk_data():
+    nltk_path = check_folder('worddumb-nltk', '', None, False)
 
     import nltk
-    if not wordnet_path.is_dir():
-        nltk.download('wordnet', download_dir=str(wordnet_path))
-    nltk.data.path.append(str(wordnet_path))
+    if not nltk_path.is_dir():
+        nltk_path = str(nltk_path)
+        nltk.download('wordnet', nltk_path)  # morphy
+        nltk.download('punkt', nltk_path)  # word_tokenize
+        nltk.download('averaged_perceptron_tagger', nltk_path)  # pos_tag
+        # ne_chunk
+        nltk.download('maxent_ne_chunker', nltk_path)
+        nltk.download('words', nltk_path)
+    nltk.data.path.append(nltk_path)
 
 
-def unzip_db(plugin_path):
-    db_path = check_folder(plugin_path, 'worddumb-db',
-                           DB_VERSION, 'dump.rdb', True)
+def unzip_db():
+    db_path = check_folder('worddumb-db', DB_VERSION, 'dump.rdb', True)
     return str(db_path.joinpath('data'))
+
+
+def download_numpy():
+    numpy_path = check_folder('worddumb-numpy', NUMPY_VERSION, None, False)
+    if not numpy_path.joinpath('numpy').is_dir():
+        import platform
+        import subprocess
+        py_version = '{}{}'.format(
+            sys.version_info.major, sys.version_info.minor)
+        pip = 'pip3'
+        if platform.system() == 'Darwin':
+            pip = '/usr/local/bin/pip3'
+        subprocess.check_call(
+            [pip, 'install', '-U', 'pip', 'setuptools', 'wheel'])
+        subprocess.check_call(
+            [pip, 'download', '-d', numpy_path, '--python-version',
+             py_version, '--no-deps', 'numpy==' + NUMPY_VERSION])
+        for wheel in numpy_path.iterdir():
+            unzip(None, numpy_path, wheel)
+            wheel.unlink()
+    sys.path.append(str(numpy_path))

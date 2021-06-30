d02e6885a79e0d17c1806b8b78f96127955e03b2
xxyzz <gitpull@protonmail.com>
2021-06-30 15:39:19 +0800

    use spacy to find named entities

--- parse_job.py
+++ parse_job.py
@@ -12,26 +12,36 @@ from calibre_plugins.worddumb.unzip import install_libs, load_json
 from calibre_plugins.worddumb.x_ray import X_Ray
 
 
-def do_job(data, install=False, abort=None, log=None, notifications=None):
-    if install:
-        install_libs()
-    (_, book_fmt, asin, book_path, _) = data
-    ll_conn = create_lang_layer(asin, book_path, book_fmt)
-    if ll_conn is None and not prefs['x-ray']:
-        return
+def do_job(data, abort=None, log=None, notifications=None):
+    (_, book_fmt, asin, book_path, _, lang) = data
+    install_libs(lang['spacy'])
+
+    create_ww = False
+    if lang['wiki'] == 'en':
+        ll_conn = create_lang_layer(asin, book_path, book_fmt)
+        if ll_conn is not None:
+            create_ww = True
+        elif not prefs['x-ray']:
+            return
+
     if prefs['x-ray']:
         if (x_ray_conn := create_x_ray_db(asin, book_path)) is None:
             return
-        x_ray = X_Ray(x_ray_conn)
-    lemmas = load_json('data/lemmas.json')
+        x_ray = X_Ray(x_ray_conn, lang['wiki'])
+        import spacy
+        nlp = spacy.load(lang['spacy'],
+                         exclude=['tok2vec', 'tagger', 'parser',
+                                  'attribute_ruler', 'lemmatizer'])
 
+    if create_ww:
+        lemmas = load_json('data/lemmas.json')
     for (start, text) in parse_book(book_path, book_fmt):
-        if ll_conn is not None:
+        if create_ww:
             find_lemma(start, text, lemmas, ll_conn)
         if prefs['x-ray']:
-            find_named_entity(start, text, x_ray)
+            find_named_entity(start, text, x_ray, nlp)
 
-    if ll_conn is not None:
+    if create_ww:
         ll_conn.commit()
         ll_conn.close()
     if prefs['x-ray']:
@@ -92,25 +102,24 @@ def find_lemma(start, text, lemmas, ll_conn):
             insert_lemma(ll_conn, (index,) + tuple(lemmas[lemma]))
 
 
-def find_named_entity(start, text, x_ray):
-    from nltk import ne_chunk, pos_tag, word_tokenize
-    from nltk.tree import Tree
-
-    records = set()
+def find_named_entity(start, text, x_ray, nlp):
     if (bytes_str := isinstance(text, bytes)):
         text = text.decode('utf-8')
-    nodes = ne_chunk(pos_tag(word_tokenize(text)))
-    for node in filter(lambda x: type(x) is Tree, nodes):
-        token = ' '.join([t for t, _ in node.leaves()])
-        if len(token) < 3 or token in records:
+
+    # https://github.com/explosion/spaCy/blob/master/spacy/glossary.py#L318
+    labels = {'EVENT', 'FAC', 'GPE', 'LANGUAGE', 'LAW', 'LOC', 'NORP', 'ORG',
+              'PERSON', 'PRODUCT', 'WORK_OF_ART', 'MISC', 'PER', 'FACILITY',
+              'ORGANIZATION', 'NAT_REL_POL',  # Romanian
+              'geogName', 'orgName', 'persName', 'placeName'}  # Polish
+    for ent in nlp(text).ents:
+        if ent.label_ not in labels:
             continue
-        records.add(token)
-        if (match := re.search(r'\b' + token + r'\b', text)) is None:
-            continue
-        index = match.start()
-        token_start = start
+        index = ent.start_char
+        ent_start = start
         if bytes_str:
-            token_start += len(text[:index].encode('utf-8'))
+            ent_start += len(text[:index].encode('utf-8'))
+            ent_len = len(ent.text.encode('utf-8'))
         else:
-            token_start += len(text[:index])
-        x_ray.search(token, node.label(), token_start, text[index:])
+            ent_start += len(text[:index])
+            ent_len = len(ent.text)
+        x_ray.search(ent.text, ent.label_, ent_start, text[index:], ent_len)

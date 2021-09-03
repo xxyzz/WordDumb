0598da41795746c241d3d89645b5a2590f14c04c
xxyzz <gitpull@protonmail.com>
2021-09-03 16:50:02 +0800

    dump flashtext object to file

---
+++ data/dump_lemmas.py
@@ -0,0 +1,30 @@
+import json
+import pickle
+
+from flashtext import KeywordProcessor
+from lemminflect import getAllInflections
+
+with open('data/lemmas.json') as f:
+    lemmas = json.load(f)
+
+keywords = set()
+keyword_processor = KeywordProcessor()
+for lemma, values in lemmas.items():
+    keyword_processor.add_keyword(lemma, values)
+    keywords.add(lemma)
+    for _, inflections in getAllInflections(lemma).items():
+        for inflection in inflections:
+            if inflection not in keywords:
+                keyword_processor.add_keyword(inflection, values)
+                keywords.add(inflection)
+    if ' ' in lemma:  # phrasae
+        words = lemma.split(' ')
+        for i, word in enumerate(words):
+            words_copy = words.copy()
+            for _, inflections in getAllInflections(word).items():
+                for inflection in inflections:
+                    words_copy[i] = inflection
+                    keyword_processor.add_keyword(' '.join(words_copy), values)
+
+with open('lemmas_dump', 'wb') as f:
+    pickle.dump(keyword_processor, f)
---
+++ data/dump_lemmas.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+pip_install() {
+    pip install flashtext lemminflect
+    mkdir libs
+    cp -R .venv/lib/python*/site-packages/flashtext* libs
+}
+
+if [[ "$OSTYPE" != "msys" ]]; then
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip_install
+    python3 data/dump_lemmas.py
+    deactivate
+else
+    py -m venv .venv
+    .\env\Scripts\activate
+    pip_install
+    py data/dump_lemmas.py
+    deactivate
+fi

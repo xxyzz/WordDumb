e9d0d0edfd681d59a744556a5e397a55769a52fd
xxyzz <gitpull@protonmail.com>
2021-10-23 21:34:42 +0800

    the origin form of phrase already included in itertools.product()
    also add lemmas with '-' replaced by ' '

--- data/dump_lemmas.py
+++ data/dump_lemmas.py
@@ -15,9 +15,6 @@ for lemma, values in lemmas.items():
     if '(' in lemma:  # 'take (something) into account'
         continue
 
-    if '/' not in lemma:
-        keyword_processor.add_keyword(lemma, values)
-
     if ' ' in lemma:  # phrase, for example: 'slick back/down'
         list_of_inflections_list = []
         for word in lemma.split(' '):
@@ -32,9 +29,13 @@ for lemma, values in lemmas.items():
                           product(*list_of_inflections_list)):
             keyword_processor.add_keyword(phrase, values)
     else:
+        keyword_processor.add_keyword(lemma, values)
         for inflection in filter(lambda x: x != lemma and x not in lemma,
                                  chain(*getAllInflections(lemma).values())):
             keyword_processor.add_keyword(inflection, values)
+
+    if '-' in lemma:
+        keyword_processor.add_keyword(lemma.replace('-', ' '), values)
 
 with open('lemmas_dump', 'wb') as f:
     pickle.dump(keyword_processor, f)

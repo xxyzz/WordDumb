af5e53bc25bc0cde08720f90621a9d7d229c7811
xxyzz <gitpull@protonmail.com>
2022-03-15 17:02:01 +0800

    Return None from load_json_or_pickle() if file doesn't exist

--- unzip.py
+++ unzip.py
@@ -7,8 +7,11 @@ import zipfile
 
 def load_json_or_pickle(plugin_path, filepath):
     with zipfile.ZipFile(plugin_path) as zf:
+        if filepath.endswith(".json"):
+            path = zipfile.Path(zf, filepath)
+            if path.exists():
+                with path.open() as f:
+                    return json.load(f)
+            return None
         with zf.open(filepath) as f:
-            if filepath.endswith('.json'):
-                return json.load(f)
-            else:
-                return pickle.load(f)
+            return pickle.load(f)

e3ef05668387199cc4cad31dcf8cb6344a768bcf
xxyzz <gitpull@protonmail.com>
2022-04-27 13:35:42 +0800

    Move error check code to error_dialogs.py

---
+++ error_dialogs.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+GITHUB_URL = "https://github.com/xxyzz/WordDumb"
+
+
+def error_dialog(title, message, error, parent):
+    from calibre.gui2.dialogs.message_box import JobError
+
+    dialog = JobError(parent)
+    dialog.msg_label.setOpenExternalLinks(True)
+    dialog.show_error(title, message, det_msg=error)
+
+
+def job_failed(job, parent=None):
+    if job and job.failed:
+        if "FileNotFoundError" in job.details and "subprocess.py" in job.details:
+            error_dialog(
+                "We want... a shrubbery!",
+                f"Please read the friendly <a href='{GITHUB_URL}#how-to-use'>manual</a> of how to install Python.",
+                job.details,
+                parent,
+            )
+        elif "CalledProcessError" in job.details:
+            subprocess_error(job, parent)
+        elif "JointMOBI" in job.details:
+            error_dialog(
+                "Joint MOBI",
+                "Please use <a href='https://github.com/kevinhendricks/KindleUnpack'>KindleUnpack</a>'s '-s' option to split the book.",
+                job.details,
+                parent,
+            )
+        elif "DLL load failed" in job.details:
+            error_dialog(
+                "Welcome to DLL Hell",
+                "Install <a href='https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads'>Visual C++ 2019 redistributable</a>",
+                job.datails,
+                parent,
+            )
+        elif "32BIT_CALIBRE" in job.details or "32BIT_PYTHON" in job.details:
+            program = "calibre" if "32BIT_CALIBRE" in job.details else "Python"
+            error_dialog(
+                "The wrist game!",
+                f"You're using 32bit {program}, please install the 64bit version.",
+                job.details,
+                parent,
+            )
+        else:
+            check_network_error(job.details, parent)
+        return True
+    return False
+
+
+def subprocess_error(job, parent):
+    exception = job.exception.stderr
+    if "No module named pip" in exception:
+        error_dialog(
+            "Hello, my name is Philip, but everyone calls me Pip, because they hate me.",
+            """
+            Run the command "sudo apt install python3-pip" to install
+            pip module if you are using Debian based distro.
+            <br><br>
+            If you still have this error, make sure you installed calibre
+            with the <a href="https://calibre-ebook.com/download_linux">
+            binary install command</a> but not from Flathub or Snap Store.
+            """,
+            job.details + exception,
+            parent,
+        )
+    else:
+        check_network_error(job.details + exception, parent)
+
+
+def check_network_error(error, parent):
+    if "check_hostname requires server_hostname" in error:
+        error_dialog(
+            "Cyberspace is not a place beyond the rule of law",
+            """
+            Check your proxy configuration environment variables,
+            they should be set by these commands:<br>
+            <code>$ export HTTP_PROXY="http://host:port"</code><br>
+            <code>$ export HTTPS_PROXY="http://host:port"</code><br>
+            <br>
+            If you're allergic to terminal, close your proxy and use a VPN.
+            """,
+            error,
+            parent,
+        )
+    elif "ConnectionError" in error or "Timeout" in error:
+        error_dialog(
+            "It was a pleasure to burn",
+            "Is GitHub/Wikipedia/Fandom blocked by your ISP? You might need tools to bypass internet censorship.",
+            error,
+            parent,
+        )
+    else:
+        error_dialog(
+            "Tonnerre de Brest!",
+            f'An error occurred, please copy error message then report bug at <a href="{GITHUB_URL}/issues">GitHub</a>.',
+            error,
+            parent,
+        )
--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class ParseBook:
             self.gui.jobs_pointer.start()
 
     def done(self, job, notif=None):
-        if self.job_failed(job):
+        if job_failed(job, self.gui):
             return
 
         book_id, _, _, mi, update_asin, book_fmt, _ = job.result
@@ -79,87 +79,3 @@ class ParseBook:
             )
         else:
             self.gui.status_bar.show_message(notif)
-
-    def job_failed(self, job):
-        if job and job.failed:
-            if "FileNotFoundError" in job.details and "subprocess.py" in job.details:
-                self.error_dialog(
-                    "We want... a shrubbery!",
-                    f"Please read the friendly <a href='{self.github_url}#how-to-use'>manual</a> of how to install Python.",
-                    job.details,
-                )
-            elif "CalledProcessError" in job.details:
-                self.subprocess_error(job)
-            elif "JointMOBI" in job.details:
-                self.error_dialog(
-                    "Joint MOBI",
-                    "Please use <a href='https://github.com/kevinhendricks/KindleUnpack'>KindleUnpack</a>'s '-s' option to split the book.",
-                    job.details,
-                )
-            elif "DLL load failed" in job.details:
-                self.error_dialog(
-                    "Welcome to DLL Hell",
-                    "Install <a href='https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads'>Visual C++ 2019 redistributable</a>",
-                    job.datails,
-                )
-            elif "32BIT_CALIBRE" in job.details or "32BIT_PYTHON" in job.details:
-                program = "calibre" if "32BIT_CALIBRE" in job.details else "Python"
-                self.error_dialog(
-                    "The wrist game!",
-                    f"You're using 32bit {program}, please install the 64bit version.",
-                    job.details,
-                )
-            else:
-                self.check_network_error(job.details)
-            return True
-        return False
-
-    def subprocess_error(self, job):
-        exception = job.exception.stderr
-        if "No module named pip" in exception:
-            self.error_dialog(
-                "Hello, my name is Philip, but everyone calls me Pip, because they hate me.",
-                """
-                Run the command "sudo apt install python3-pip" to install
-                pip module if you are using Debian based distro.
-                <br><br>
-                If you still have this error, make sure you installed calibre
-                with the <a href="https://calibre-ebook.com/download_linux">
-                binary install command</a> but not from Flathub or Snap Store.
-                """,
-                job.details + exception,
-            )
-        else:
-            self.check_network_error(job.details + exception)
-
-    def error_dialog(self, title, message, error):
-        dialog = JobError(self.gui)
-        dialog.msg_label.setOpenExternalLinks(True)
-        dialog.show_error(title, message, det_msg=error)
-
-    def check_network_error(self, error):
-        if "check_hostname requires server_hostname" in error:
-            self.error_dialog(
-                "Cyberspace is not a place beyond the rule of law",
-                """
-                Check your proxy configuration environment variables,
-                they should be set by these commands:<br>
-                <code>$ export HTTP_PROXY="http://host:port"</code><br>
-                <code>$ export HTTPS_PROXY="http://host:port"</code><br>
-                <br>
-                If you're allergic to terminal, close your proxy and use a VPN.
-                """,
-                error,
-            )
-        elif "ConnectionError" in error or "Timeout" in error:
-            self.error_dialog(
-                "It was a pleasure to burn",
-                "Is GitHub/Wikipedia/Fandom blocked by your ISP? You might need tools to bypass internet censorship.",
-                error,
-            )
-        else:
-            self.error_dialog(
-                "Tonnerre de Brest!",
-                f'An error occurred, please copy error message then report bug at <a href="{self.github_url}/issues">GitHub</a>.',
-                error,
-            )

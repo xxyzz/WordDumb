82f3fd2b5ad8de330c37a2dac6f1b12f4f444618
xxyzz <gitpull@protonmail.com>
2022-11-27 20:47:47 +0800

    Use MediaWiki's parse API to get text for redirect to section pages

--- mediawiki.py
+++ mediawiki.py
@@ -4,10 +4,12 @@ import json
 import re
 from collections import defaultdict
 from datetime import datetime
+from pathlib import Path
+from typing import Any
 from urllib.parse import unquote
 
 # https://www.mediawiki.org/wiki/API:Get_the_contents_of_a_page
-# https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:TextExtracts#API
+# https://www.mediawiki.org/wiki/Extension:TextExtracts#API
 MEDIAWIKI_API_EXLIMIT = 20
 FUZZ_THRESHOLD = 85.7
 
@@ -46,7 +48,7 @@ GPE_LABELS = frozenset(["GPE", "GPE_LOC", "GPE_ORG", "placeName", "LC"])
 
 
 class MediaWiki:
-    def __init__(self, cache_path, useragent):
+    def __init__(self, cache_path: Path, useragent: str) -> None:
         import requests
 
         self.cache_path = cache_path
@@ -61,23 +63,23 @@ class MediaWiki:
         self.session = requests.Session()
         self.session.headers.update({"user-agent": useragent})
 
-    def get_cache(self, key):
+    def get_cache(self, key: str) -> Any:
         data = self.get_direct_cache(key)
         if isinstance(data, str):
             return self.get_direct_cache(data)
         return data
 
-    def get_direct_cache(self, key):
+    def get_direct_cache(self, key: str) -> Any:
         return self.cache.get(key)
 
-    def add_cache(self, key, value):
+    def add_cache(self, key: str, value: Any) -> None:
         self.cache[key] = value
         self.save_cache = True
 
-    def has_cache(self, key):
+    def has_cache(self, key: str) -> bool:
         return key in self.cache
 
-    def close(self):
+    def close(self) -> None:
         self.session.close()
         if self.save_cache:
             with self.cache_path.open("w") as f:
@@ -85,7 +87,9 @@ class MediaWiki:
 
 
 class Wikipedia(MediaWiki):
-    def __init__(self, lang, useragent, plugin_path, prefs):
+    def __init__(
+        self, lang: str, useragent: str, plugin_path: Path, prefs: dict[str, str]
+    ) -> None:
         self.lang = lang
         self.source_id = 1
         self.source_name = "Wikipedia"
@@ -98,28 +102,33 @@ class Wikipedia(MediaWiki):
         cache_path = plugin_path.parent.joinpath(f"worddumb-wikimedia/{lang}.json")
 
         super().__init__(cache_path, useragent)
-        self.session.params = {
-            "format": "json",
-            "action": "query",
-            "prop": "extracts|pageprops",
-            "exintro": 1,
-            "explaintext": 1,
-            "redirects": 1,
-            "exsentences": 7,
-            "formatversion": 2,
-            "ppprop": "wikibase_item",
-        }
-        if lang == "zh" and not prefs["fandom"]:
+        self.session.params = {"format": "json", "formatversion": 2}
+        if lang == "zh":
             self.session.params["variant"] = f"zh-{prefs['zh_wiki_variant']}"
 
-    def query(self, titles):
-        result = self.session.get(self.wiki_api, params={"titles": "|".join(titles)})
+    def query(self, titles: set[str]) -> None:
+        result = self.session.get(
+            self.wiki_api,
+            params={
+                "action": "query",
+                "prop": "extracts|pageprops",
+                "exintro": 1,
+                "explaintext": 1,
+                "redirects": 1,
+                "exsentences": 7,
+                "ppprop": "wikibase_item",
+                "titles": "|".join(titles),
+            },
+        )
         data = result.json()
         converts = defaultdict(list)
+        redirect_to_sections = defaultdict(dict)
         for convert_type in ["normalized", "redirects"]:
             for d in data["query"].get(convert_type, []):
                 # different titles can be redirected to the same page
                 converts[d["to"]].append(d["from"])
+                if "tofragment" in d:
+                    redirect_to_sections[d["to"]][d["tofragment"]] = d["from"]
 
         for v in data["query"]["pages"]:
             if "extract" not in v:  # missing or invalid
@@ -127,6 +136,8 @@ class Wikipedia(MediaWiki):
             # they are ordered by pageid, ehh
             title = v["title"]
             summary = v["extract"]
+            if title in redirect_to_sections and title not in titles:
+                continue
             if not any(period in summary for period in [".", "。"]):
                 continue  # very likely a disambiguation page
             self.add_cache(
@@ -147,12 +158,68 @@ class Wikipedia(MediaWiki):
                     if k in titles:  # normalize then redirect
                         titles.remove(k)
 
+        self.get_section_text(redirect_to_sections, converts, titles)
         for title in titles:  # use quote next time
             self.add_cache(title, None)
 
+    def get_section_text(
+        self,
+        redirect_to_sections: dict[dict[str, str]],
+        converts: dict[str, list[str]],
+        titles: set[str],
+    ) -> None:
+        from lxml import etree
+
+        for page, section_to_titles in redirect_to_sections.items():
+            r = self.session.get(
+                self.wiki_api,
+                params={
+                    "action": "parse",
+                    "prop": "sections",
+                    "page": page,
+                },
+            )
+            result = r.json()
+            for section in result.get("parse", {}).get("sections", []):
+                if section["line"] in section_to_titles:
+                    r = self.session.get(
+                        self.wiki_api,
+                        params={
+                            "action": "parse",
+                            "prop": "text",
+                            "section": section["index"],
+                            "disabletoc": 1,
+                            "disableeditsection": 1,
+                            "disablelimitreport": 1,
+                            "page": page,
+                        },
+                    )
+                    section_result = r.json()
+                    html_text = section_result.get("parse", {}).get("text")
+                    if not html_text:
+                        continue
+                    html = etree.HTML(html_text)
+                    # Remove references
+                    for e in html.xpath("//p[1]/sup[contains(@class, 'reference')]"):
+                        e.getparent().remove(e)
+                    text = e.xpath("string(//p[1])")
+                    if not text:
+                        continue
+                    text = text.strip()
+                    redirected_title = section_to_titles[section["line"]]
+                    self.add_cache(redirected_title, text)
+                    if redirected_title in titles:
+                        titles.remove(redirected_title)
+                    for converted_title in converts.get(redirected_title, []):
+                        self.add_cache(converted_title, text)
+                        if converted_title in titles:
+                            titles.remove(converted_title)
+
 
 class Fandom(MediaWiki):
-    def __init__(self, useragent, plugin_path, prefs):
+    def __init__(
+        self, useragent: str, plugin_path: Path, prefs: dict[str, str]
+    ) -> None:
         self.source_id = 2
         self.source_name = "Fandom"
         self.source_link = f"{prefs['fandom']}/wiki/"
@@ -164,17 +231,18 @@ class Fandom(MediaWiki):
 
         super().__init__(cache_path, useragent)
         # Fandom doesn't have TextExtract extension
-        # https://www.mediawiki.org/wiki/Special:MyLanguage/API:Parse
+        # https://www.mediawiki.org/wiki/API:Parse
         self.session.params = {
             "format": "json",
             "action": "parse",
             "prop": "text",
             "section": 0,
             "redirects": 1,
+            "disablelimitreport": 1,
             "formatversion": 2,
         }
 
-    def query(self, page):
+    def query(self, page: str) -> None:
         from lxml import etree
 
         result = self.session.get(self.wiki_api, params={"page": page})
@@ -195,37 +263,37 @@ class Fandom(MediaWiki):
 
 
 class Wikimedia_Commons:
-    def __init__(self, plugin_path, useragent):
+    def __init__(self, plugin_path: str, useragent: str) -> None:
         import requests
 
         self.session = requests.Session()
         self.session.headers.update({"user-agent": useragent})
         self.cache_folder = plugin_path.parent.joinpath("worddumb-wikimedia")
 
-    def get_image(self, filename):
+    def get_image(self, filename: str) -> Path:
         file_path = self.cache_folder.joinpath(filename)
         if not file_path.exists():
             self.download_image(filename, file_path)
         return file_path
 
-    def download_image(self, filename, file_path):
+    def download_image(self, filename: str, file_path: Path) -> None:
         r = self.session.get(
             f"https://commons.wikimedia.org/wiki/Special:FilePath/{filename}"
         )
         with file_path.open("wb") as f:
             f.write(r.content)
 
-    def close(self):
+    def close(self) -> None:
         self.session.close()
 
 
 class Wikidata(MediaWiki):
-    def __init__(self, plugin_path, useragent):
+    def __init__(self, plugin_path: str, useragent: str) -> None:
         super().__init__(
             plugin_path.parent.joinpath("worddumb-wikimedia/wikidata.json"), useragent
         )
 
-    def query(self, items):
+    def query(self, items: list[str]) -> None:
         items = " ".join(map(lambda x: f"wd:{x}", items))
         query = f"""
         SELECT ?item (SAMPLE(?maps) AS ?map) (MAX(?inceptions) AS ?inception) WHERE {{
@@ -264,7 +332,7 @@ class Wikidata(MediaWiki):
                 self.add_cache(item_id, None)
 
 
-def inception_text(inception):
+def inception_text(inception: str) -> str:
     if inception.startswith("-"):
         bc = int(inception[1:5]) + 1  # 2BC: -0001, 1BC: +0000, 1AD: 0001
         years = datetime.now().year + bc
@@ -277,8 +345,10 @@ def inception_text(inception):
         return f"Inception: {inception.strftime('%d %B %Y').lstrip('0')}({years} years ago)"
 
 
-def query_mediawiki(entities, mediawiki, search_people):
-    pending_entities = []
+def query_mediawiki(
+    entities: dict[str, dict[str, str]], mediawiki: MediaWiki, search_people: bool
+) -> None:
+    pending_entities = set()
     for entity, data in entities.items():
         if (
             isinstance(mediawiki, Wikipedia)
@@ -290,7 +360,7 @@ def query_mediawiki(entities, mediawiki, search_people):
             search_people or data["label"] not in PERSON_LABELS
         ):
             if isinstance(mediawiki, Wikipedia):
-                pending_entities.append(entity)
+                pending_entities.add(entity)
             else:
                 mediawiki.query(entity)
     if len(pending_entities):
@@ -298,7 +368,9 @@ def query_mediawiki(entities, mediawiki, search_people):
     mediawiki.close()
 
 
-def query_wikidata(entities, mediawiki, wikidata):
+def query_wikidata(
+    entities: dict[str, dict[str, str]], mediawiki: MediaWiki, wikidata: Wikidata
+) -> None:
     pending_item_ids = []
     for item_id in filter(
         lambda x: x and not wikidata.has_cache(x),
@@ -330,7 +402,9 @@ def is_gpe_label(lang: str, label: str) -> bool:
 NAME_DIVISION_REG = r"\s|\u00B7|\u2027|\u30FB|\uFF65"
 
 
-def is_full_name(partial_name, partial_label, full_name, full_label):
+def is_full_name(
+    partial_name: str, partial_label: str, full_name: str, full_label: str
+) -> bool:
     return (
         not re.search(NAME_DIVISION_REG, partial_name)
         and re.search(NAME_DIVISION_REG, full_name)

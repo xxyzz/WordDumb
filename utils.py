cdbaeef3e02e560a5ed9a15cda92a68f6b6502ce
xxyzz <gitpull@protonmail.com>
2023-07-23 19:00:58 +0800

    v3.29.4

--- utils.py
+++ utils.py
@@ -13,7 +13,9 @@ from typing import Any, TypedDict
 
 CJK_LANGS = ["zh", "ja", "ko"]
 PROFICIENCY_VERSION = "0.5.8"
-PROFICIENCY_RELEASE_URL = f"https://github.com/xxyzz/Proficiency/releases/download/v{PROFICIENCY_VERSION}"
+PROFICIENCY_RELEASE_URL = (
+    f"https://github.com/xxyzz/Proficiency/releases/download/v{PROFICIENCY_VERSION}"
+)
 PROFICIENCY_MAJOR_VERSION = PROFICIENCY_VERSION.split(".", 1)[0]
 
 

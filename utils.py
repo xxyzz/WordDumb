7fbb131f615de7ad9c1b797ceef18897e1f58ca7
xxyzz <gitpull@protonmail.com>
2024-01-02 22:18:25 +0800

    Update proficiency v0.5.12

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from typing import Any, TypedDict
 
 CJK_LANGS = ["zh", "ja", "ko"]
-PROFICIENCY_VERSION = "0.5.11"
+PROFICIENCY_VERSION = "0.5.12"
 PROFICIENCY_RELEASE_URL = (
     f"https://github.com/xxyzz/Proficiency/releases/download/v{PROFICIENCY_VERSION}"
 )
